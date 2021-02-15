9fe1c5ebbc11fbd8d2f0a1a47319a080c326eb62
Eric Lingren <ericlingren@gmail.com>
2021-02-14 21:48:56 -0700

    testing bug fixes

--- autostart_all_bots.sh
+++ autostart_all_bots.sh
@@ -8,9 +8,9 @@ EMAIL_TO=$4
 GMAIL_SERVER_ACCOUNT=$5
 GMAIL_SERVER_PASSWORD=$6
 
-echo $OANDA_ACCOUNT_NUMBER
-echo $OANDA_TOKEN
-echo $BOT_NAME
+# echo $OANDA_ACCOUNT_NUMBER
+# echo $OANDA_TOKEN
+# echo $BOT_NAME
 
 cd ../environments
 # cd environments
@@ -22,11 +22,12 @@ for dir in ./*/; do
             (
                 cd "$subdir"  
                 if test -f "main.py"; then
-                    pwd
+                    # pwd
                     FOLDER_NAME=$(echo $dir | cut -d'/' -f 2)
                     CURRENCY_PAIR=$(echo $FOLDER_NAME | cut -d'-' -f 2)
 
-                    BOT_SCRIPT_EXECUTION_STRING=$(echo nohup python3 main.py --oanda_account $OANDA_ACCOUNT_NUMBER --oanda_token $OANDA_TOKEN --pair $CURRENCY_PAIR --bot $BOT_NAME --email_to $EMAIL_TO --gmail_server_account $GMAIL_SERVER_ACCOUNT --gmail_server_password $GMAIL_SERVER_PASSWORD 2> /dev/null &)
+                    BOT_SCRIPT_EXECUTION_STRING=$(echo nohup python3 main.py --oanda_account $OANDA_ACCOUNT_NUMBER --oanda_token $OANDA_TOKEN --pair $CURRENCY_PAIR --bot $BOT_NAME --email_to $EMAIL_TO --gmail_server_account $GMAIL_SERVER_ACCOUNT --gmail_server_password $GMAIL_SERVER_PASSWORD &)
+                    echo -ne '\n'
                     echo $BOT_SCRIPT_EXECUTION_STRING
                     sudo chmod u+x main.py
                     $BOT_SCRIPT_EXECUTION_STRING
