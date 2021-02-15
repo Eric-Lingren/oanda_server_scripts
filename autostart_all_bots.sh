24170c0af8c1e0b7c23a63b0654b2fd59b13449e
Eric Lingren <ericlingren@gmail.com>
2021-02-14 21:31:17 -0700

    adding autostart script

---
+++ autostart_all_bots.sh
@@ -0,0 +1,40 @@
+
+#!/bin/bash
+my_custom_tab="          "
+OANDA_ACCOUNT_NUMBER=$1
+OANDA_TOKEN=$2
+OANDA_TOKEN=$3
+EMAIL_TO=$4
+GMAIL_SERVER_ACCOUNT=$5
+GMAIL_SERVER_PASSWORD=$6
+
+cd ../environments
+# cd environments
+
+for dir in ./*/; do 
+    (
+        cd "$dir"  
+        for subdir in ./*/; do 
+            (
+                cd "$subdir"  
+                if test -f "main.py"; then
+                    pwd
+                    FOLDER_NAME=$(echo $dir | cut -d'/' -f 2)
+                    CURRENCY_PAIR=$(echo $FOLDER_NAME | cut -d'-' -f 2)
+
+                    BOT_SCRIPT_EXECUTION_STRING=$(echo nohup python3 main.py --oanda_account $OANDA_ACCOUNT_NUMBER --oanda_token $OANDA_TOKEN --pair $CURRENCY_PAIR --bot $BOT_NAME --email_to $EMAIL_TO --gmail_server_account $GMAIL_SERVER_ACCOUNT --gmail_server_password $GMAIL_SERVER_PASSWORD &)
+                    echo $BOT_SCRIPT_EXECUTION_STRING
+                    $BOT_SCRIPT_EXECUTION_STRING
+                    echo "-----------------------------------------------------------"
+                    echo "|${my_custom_tab}THE FOLLWING ROBOT HAS BEEN STARTED:${my_custom_tab}|"
+                    echo "${my_custom_tab}${my_custom_tab} $FOLDER_NAME ${my_custom_tab}${my_custom_tab}"
+                    echo "-----------------------------------------------------------"
+                    echo " "
+                fi
+            ); 
+        done
+    ); 
+    done
+
+cd ..
+echo " "
